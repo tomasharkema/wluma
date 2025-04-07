1adc90952f28fb8b163c4a8e5ebd486587bafd5b
tomasharkema <tomas@harkema.io>
2025-04-07 14:42:30 +0200

    updates

--- flake.nix
+++ flake.nix
@@ -5,20 +5,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    let systems = [ "x86_64-linux" "aarch64-linux" ];
-    in flake-utils.lib.eachSystem systems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+  in
+    flake-utils.lib.eachSystem systems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        naersk-lib = pkgs.callPackage naersk {};
         libs = with pkgs; [
           udev
           v4l-utils
           vulkan-loader
           dbus
         ];
-      in
-      {
+      in {
         defaultPackage = naersk-lib.buildPackage {
           src = ./.;
           meta.mainProgram = "wluma";
@@ -29,22 +34,26 @@
             marked-man
           ];
           buildInputs = libs;
+          env.WLUMA_VERSION = "0.0.0-local";
         };
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config ] ++ libs;
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath [ wayland ]}";
-          VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
-          VK_INSTANCE_LAYERS = "VK_LAYER_KHRONOS_validation";
-          VK_LOADER_DEBUG = "error,warn";
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
-          BINDGEN_EXTRA_CLANG_ARGS = [
-            ''-I"${llvmPackages_12.libclang.lib}/lib/clang/${llvmPackages_12.libclang.version}/include"''
-          ] ++ (builtins.map (a: ''-I"${a}/include"'') [
-            glibc.dev
-            libv4l.dev
-          ]);
-        };
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config] ++ libs;
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath [wayland]}";
+            VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            VK_INSTANCE_LAYERS = "VK_LAYER_KHRONOS_validation";
+            VK_LOADER_DEBUG = "error,warn";
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            LIBCLANG_PATH = "${llvmPackages_12.libclang.lib}/lib";
+            BINDGEN_EXTRA_CLANG_ARGS =
+              [
+                ''-I"${llvmPackages_12.libclang.lib}/lib/clang/${llvmPackages_12.libclang.version}/include"''
+              ]
+              ++ (builtins.map (a: ''-I"${a}/include"'') [
+                glibc.dev
+                libv4l.dev
+              ]);
+          };
       }
     );
 }
